bf9d78dd92ad8b4b6b4b8f2ea1c99ecf90035220
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-05-28 09:06:42 +0900

    update

--- debug/main.go
+++ debug/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"rfc9401"
 )
 
 func main() {
-	tcpHeader, err := rfc9401.Dial("127.0.0.1", "127.0.0.1", 18080)
+	fd, tcpHeader, err := rfc9401.Dial("127.0.0.1", "127.0.0.1", 18080)
 	if err != nil {
 		log.Fatalf("Client connection is error : %v", err)
 	}
-	err = tcpHeader.Write([]byte(`hoge`))
+	fmt.Printf("tcpHeader is %+v\n", tcpHeader)
+	err = tcpHeader.Write(fd, []byte{0x68, 0x6f, 0x67, 0x65, 0x0a})
 	if err != nil {
 		log.Fatalf("Write error : %v", err)
 	}
