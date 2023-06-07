6afc14ca4cce2fcd8661d2cf7fdcd19b2490f420
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-06-07 09:00:42 +0900

    updata example programs

--- example/server.go
+++ example/server.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	tcpstate := make(chan rfc9401.TcpState)
-	rfc9401.Listen("127.0.0.1", 18000, tcpstate)
+	//tcpstate := make(chan rfc9401.TcpState)
+	//rfc9401.Listen("127.0.0.1", 18000, tcpstate)
 
-	//rfc9401.ListenAndServeHTTP("127.0.0.1", 18000)
+	rfc9401.ListenAndServeHTTP("127.0.0.1", 18000)
 }
